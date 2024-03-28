485a3516f0d1d27e4391639787d62e87e60d4c83
Lasma Poksane <laasmapoksane@gmail.com>
2024-03-28 22:26:44 +0100

    add custom color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      white: "#F0F0F0",
+      black: "#333333",
+      "hot-pink": "#B0578D",
+      pink: "#D988B9",
+      "light-pink": "#FACBEA",
+      beige: "#FFE4D6",
+    },
     extend: {},
   },
   plugins: [],

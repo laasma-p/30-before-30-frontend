48dc3035748c9516f3579aef65eb12c1d172f1c7
Lasma Poksane <laasmapoksane@gmail.com>
2024-03-30 12:52:09 +0100

    make inputs controllable

--- src/Login.jsx
+++ src/Login.jsx
@@ -2,9 +2,19 @@ import { useState } from "react";
 
 const Login = () => {
   const [openForm, setOpenForm] = useState(false);
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredPassword, setEnteredPassword] = useState("");
 
   const toggleFormHandler = () => {
     setOpenForm(!openForm);
+  };
+
+  const enteredEmailChangeHandler = (event) => {
+    setEnteredEmail(event.target.value);
+  };
+
+  const enteredPasswordChangeHandler = (event) => {
+    setEnteredPassword(event.target.value);
   };
 
   return (
@@ -27,6 +37,8 @@ const Login = () => {
               id="email"
               type="email"
               className="w-full rounded-md max-w-xs py-1.5 px-2 focus:outline-hot-pink text-md"
+              value={enteredEmail}
+              onChange={enteredEmailChangeHandler}
             />
             <label htmlFor="email" className="block my-2 text-md">
               Password
@@ -35,6 +47,8 @@ const Login = () => {
               id="password"
               type="password"
               className="w-full rounded-md max-w-xs py-1.5 px-2 focus:outline-hot-pink text-md"
+              value={enteredPassword}
+              onChange={enteredPasswordChangeHandler}
             />
             <button className="bg-hot-pink hover:bg-pink text-white hover:text-black dark:bg-pink dark:hover:bg-hot-pink dark:hover:text-black w-full text-lg max-w-xs px-1.5 py-2 rounded-md mt-4">
               Log In

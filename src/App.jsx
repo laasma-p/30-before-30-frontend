9fd4db769bc5352148acabd42f5a8a99135bc7ac
Lasma Poksane <laasmapoksane@gmail.com>
2024-03-30 05:28:49 +0100

    fetch items from database

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,28 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./Header";
 import Login from "./Login";
 import List from "./List";
 import ProgressBar from "./ProgressBar";
 import Footer from "./Footer";
 
-const listItems = [
-  {
-    id: 1,
-    itemName: "Item 1",
-  },
-  {
-    id: 2,
-    itemName: "Item 2",
-  },
-  {
-    id: 3,
-    itemName: "Item 3",
-  },
-  {
-    id: 4,
-    itemName: "Item 4",
-  },
-  {
-    id: 5,
-    itemName: "Item 5",
-  },
-  {
-    id: 6,
-    itemName: "Item 6",
-  },
-  {
-    id: 7,
-    itemName: "Item 7",
-  },
-  {
-    id: 8,
-    itemName: "Item 8",
-  },
-  {
-    id: 9,
-    itemName: "Item 9",
-  },
-];
-
 function App() {
+  const [listItems, setListItems] = useState([]);
   const [completedItems, setCompletedItems] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/items")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setListItems(data);
+      })
+      .catch((error) => {
+        console.log("Error fetching items: ", error);
+      });
+  }, []);
+
   const totalItems = listItems.length;
   const progress = (completedItems.length / totalItems) * 100;
 

a4afc6df02576f354d204186f64831074c93813f
Lasma Poksane <laasmapoksane@gmail.com>
2024-05-11 00:01:07 +0200

    re-work App component to implement checking the token's persistence

--- src/App.jsx
+++ src/App.jsx
@@ -3,46 +3,28 @@ import { useState, useEffect } from "react";
 import Header from "./Header";
 import Login from "./Login";
 import List from "./List";
-import ProgressBar from "./ProgressBar";
 import Footer from "./Footer";
 
 function App() {
-  const [listItems, setListItems] = useState([]);
-  const [completedItems, setCompletedItems] = useState([]);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
-    fetch("http://localhost:3000/items")
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        setListItems(data);
-      })
-      .catch((error) => {
-        console.log("Error fetching items: ", error);
-      });
+    const token = localStorage.getItem("token");
+    setIsLoggedIn(token !== null);
+
+    const storageChangeHandler = () => {
+      const updatedToken = localStorage.getItem("token");
+      setIsLoggedIn(updatedToken !== null);
+    };
+
+    window.addEventListener("storage", storageChangeHandler);
+    return () => {
+      window.removeEventListener("storage", storageChangeHandler);
+    };
   }, []);
 
   const handleLogin = () => {
     setIsLoggedIn(true);
-  };
-
-  const handleLogout = () => {
-    setIsLoggedIn(false);
-  };
-
-  const totalItems = listItems.length;
-  const progress = (completedItems.length / totalItems) * 100;
-
-  const strikeItemCompletion = (itemId) => {
-    setCompletedItems((prevCompletedItems) => {
-      if (prevCompletedItems.includes(itemId)) {
-        return prevCompletedItems.filter((id) => id !== itemId);
-      } else {
-        return [...prevCompletedItems, itemId];
-      }
-    });
   };
 
   return (
@@ -53,14 +35,8 @@ function App() {
           isLoggedIn={isLoggedIn}
           setIsLoggedIn={setIsLoggedIn}
           onLogin={handleLogin}
-          onLogout={handleLogout}
         />
-        <ProgressBar progress={progress} />
-        <List
-          listItems={listItems}
-          completedItems={completedItems}
-          strikeItemCompletion={strikeItemCompletion}
-        />
+        <List isLoggedIn={isLoggedIn} />
       </div>
       <Footer />
     </div>

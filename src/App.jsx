b24891d6aa3f16614ad4609b0aefdc759e6d4fdf
Lasma Poksane <laasmapoksane@gmail.com>
2024-05-16 14:59:02 +0200

    add class to smoothly change from light to dark or vice versa themes

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
   };
 
   return (
-    <div className="flex flex-col min-h-screen bg-white dark:bg-black">
+    <div className="flex flex-col min-h-screen bg-white dark:bg-black transition-all">
       <Header />
       <div className="flex-grow">
         <Login

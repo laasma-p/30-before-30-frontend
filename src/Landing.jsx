22bd3460cb39aafa2071247aca5c13fb23e5ffeb
Lasma Poksane <laasmapoksane@gmail.com>
2024-05-16 15:39:27 +0200

    add missing button

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -6,6 +6,7 @@ const Landing = () => {
         Here you can keep your list of 30 things YOU want to do before YOU turn
         30, track progress and see how far have you come.
       </p>
+      <button type="button">Open form</button>
     </div>
   );
 };

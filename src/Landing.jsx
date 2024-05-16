aacd3ce6b930dcecb41d551c5fee258936f46597
Lasma Poksane <laasmapoksane@gmail.com>
2024-05-16 14:39:56 +0200

    create initial Landing component

---
+++ src/Landing.jsx
@@ -0,0 +1,13 @@
+const Landing = () => {
+  return (
+    <div>
+      <h1>30 Before 30</h1>
+      <p>
+        Here you can keep your list of 30 things YOU want to do before YOU turn
+        30, track progress and see how far have you come.
+      </p>
+    </div>
+  );
+};
+
+export default Landing;

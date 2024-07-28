26ff70e65417e584717fd810c60899a63c96dbbe
Lasma Poksane <laasmapoksane@gmail.com>
2024-07-28 14:43:34 +0200

    render Landing component

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,11 @@
 import "./App.css";
-import { useState, useEffect } from "react";
-import Header from "./Header";
-import Form from "./Form";
-import List from "./List";
+import Landing from "./Landing";
 import Footer from "./Footer";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    setIsLoggedIn(token !== null);
-
-    const storageChangeHandler = () => {
-      const updatedToken = localStorage.getItem("token");
-      setIsLoggedIn(updatedToken !== null);
-    };
-
-    window.addEventListener("storage", storageChangeHandler);
-    return () => {
-      window.removeEventListener("storage", storageChangeHandler);
-    };
-  }, []);
-
-  const handleLogin = () => {
-    setIsLoggedIn(true);
-  };
-
   return (
     <div className="flex flex-col min-h-screen bg-white dark:bg-black transition-all">
-      <Header />
-      <div className="flex-grow">
-        <Form
-          isLoggedIn={isLoggedIn}
-          setIsLoggedIn={setIsLoggedIn}
-          onLogin={handleLogin}
-        />
-        <List isLoggedIn={isLoggedIn} />
-      </div>
+      <Landing />
       <Footer />
     </div>
   );

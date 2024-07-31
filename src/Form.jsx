69234fd98a63d63700ef0970a2f33d3de5698773
Lasma Poksane <laasmapoksane@gmail.com>
2024-08-01 00:00:14 +0200

    add register form that can be switched from login

--- src/Form.jsx
+++ src/Form.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 const Form = ({ isLoggedIn, setIsLoggedIn, onLogin }) => {
   const [enteredEmail, setEnteredEmail] = useState("");
   const [enteredPassword, setEnteredPassword] = useState("");
+  const [enteredName, setEnteredName] = useState("");
   const [isLoginForm, setIsLoginForm] = useState(false);
 
   const enteredEmailChangeHandler = (event) => {
@@ -11,6 +12,10 @@ const Form = ({ isLoggedIn, setIsLoggedIn, onLogin }) => {
 
   const enteredPasswordChangeHandler = (event) => {
     setEnteredPassword(event.target.value);
+  };
+
+  const enteredNameChangeHandler = (event) => {
+    setEnteredName(event.target.value);
   };
 
   const switchFormHandler = () => {
@@ -41,6 +46,30 @@ const Form = ({ isLoggedIn, setIsLoggedIn, onLogin }) => {
       onLogin(data.listData);
     } catch (error) {
       console.error("Error logging in:", error);
+    }
+  };
+
+  const registerHandler = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await fetch("http://localhost:3000/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ enteredName, enteredEmail, enteredPassword }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Registration failed");
+      }
+
+      setEnteredName("");
+      setEnteredEmail("");
+      setEnteredPassword("");
+    } catch (error) {
+      console.error("Error registering:", error);
     }
   };
 
@@ -80,12 +109,51 @@ const Form = ({ isLoggedIn, setIsLoggedIn, onLogin }) => {
             <button className="bg-hot-pink hover:bg-pink text-white hover:text-black dark:bg-pink dark:hover:bg-hot-pink dark:hover:text-black w-full text-lg max-w-xs px-1.5 py-2 rounded-md mt-4">
               Log In
             </button>
-            <button onClick={switchFormHandler}>Switch to register</button>
+            <button className="mt-2" onClick={switchFormHandler}>
+              Switch to register
+            </button>
           </form>
         ) : (
-          <form className="flex flex-col justify-center items-center pt-1 pb-4">
-            <p>Register Form</p>
-            <button onClick={switchFormHandler}>Switch to login</button>
+          <form
+            className="flex flex-col justify-center items-center pt-1 pb-4"
+            onSubmit={registerHandler}
+          >
+            <label htmlFor="name" className="block my-2 text-md">
+              Name
+            </label>
+            <input
+              id="name"
+              type="text"
+              className="w-full rounded-md max-w-xs py-1.5 px-2 focus:outline-hot-pink text-md"
+              value={enteredName}
+              onChange={enteredNameChangeHandler}
+            />
+            <label htmlFor="email" className="block my-2 text-md">
+              E-mail
+            </label>
+            <input
+              id="email"
+              type="email"
+              className="w-full rounded-md max-w-xs py-1.5 px-2 focus:outline-hot-pink text-md"
+              value={enteredEmail}
+              onChange={enteredEmailChangeHandler}
+            />
+            <label htmlFor="password" className="block my-2 text-md">
+              Password
+            </label>
+            <input
+              id="password"
+              type="password"
+              className="w-full rounded-md max-w-xs py-1.5 px-2 focus:outline-hot-pink text-md"
+              value={enteredPassword}
+              onChange={enteredPasswordChangeHandler}
+            />
+            <button className="bg-hot-pink hover:bg-pink text-white hover:text-black dark:bg-pink dark:hover:bg-hot-pink dark:hover:text-black w-full text-lg max-w-xs px-1.5 py-2 rounded-md mt-4">
+              Register
+            </button>
+            <button className="mt-2" onClick={switchFormHandler}>
+              Switch to login
+            </button>
           </form>
         )}
       </div>

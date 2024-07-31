4b3b4fdbf69393edde013822fe6bf8dd7d455653
Lasma Poksane <laasmapoksane@gmail.com>
2024-07-31 22:21:58 +0200

    move opening form logic to Landing component

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,13 +1,8 @@
 import { useState } from "react";
 
 const Form = ({ isLoggedIn, setIsLoggedIn, onLogin }) => {
-  const [openForm, setOpenForm] = useState(false);
   const [enteredEmail, setEnteredEmail] = useState("");
   const [enteredPassword, setEnteredPassword] = useState("");
-
-  const toggleFormHandler = () => {
-    setOpenForm(!openForm);
-  };
 
   const enteredEmailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
@@ -36,7 +31,6 @@ const Form = ({ isLoggedIn, setIsLoggedIn, onLogin }) => {
       const data = await response.json();
       localStorage.setItem("token", data.token);
       setIsLoggedIn(true);
-      setOpenForm(false);
       setEnteredEmail("");
       setEnteredPassword("");
       onLogin(data.listData);
@@ -51,47 +45,37 @@ const Form = ({ isLoggedIn, setIsLoggedIn, onLogin }) => {
   };
 
   return (
-    <div className="flex flex-col transition-all justify-center items-center">
-      {!isLoggedIn && !openForm && (
-        <button
-          className="transition-all bg-hot-pink hover:bg-pink text-white hover:text-black dark:bg-pink dark:hover:bg-hot-pink dark:hover:text-black w-10/12 text-lg max-w-xs px-1.5 py-2 rounded-md mt-4"
-          onClick={toggleFormHandler}
+    <>
+      <div className="transition-all bg-light-pink w-10/12 sm:max-w-xl mx-auto mt-4 mb-2 px-4 rounded-md">
+        <form
+          className="flex flex-col justify-center items-center pt-1 pb-4"
+          onSubmit={loginHandler}
         >
-          Open form
-        </button>
-      )}
-      {!isLoggedIn && openForm && (
-        <div className="transition-all bg-light-pink w-10/12 sm:max-w-xl mx-auto mt-4 mb-2 px-4 rounded-md">
-          <form
-            className="flex flex-col justify-center items-center pt-1 pb-4"
-            onSubmit={loginHandler}
-          >
-            <label htmlFor="email" className="block my-2 text-md">
-              E-mail
-            </label>
-            <input
-              id="email"
-              type="email"
-              className="w-full rounded-md max-w-xs py-1.5 px-2 focus:outline-hot-pink text-md"
-              value={enteredEmail}
-              onChange={enteredEmailChangeHandler}
-            />
-            <label htmlFor="email" className="block my-2 text-md">
-              Password
-            </label>
-            <input
-              id="password"
-              type="password"
-              className="w-full rounded-md max-w-xs py-1.5 px-2 focus:outline-hot-pink text-md"
-              value={enteredPassword}
-              onChange={enteredPasswordChangeHandler}
-            />
-            <button className="bg-hot-pink hover:bg-pink text-white hover:text-black dark:bg-pink dark:hover:bg-hot-pink dark:hover:text-black w-full text-lg max-w-xs px-1.5 py-2 rounded-md mt-4">
-              Log In
-            </button>
-          </form>
-        </div>
-      )}
+          <label htmlFor="email" className="block my-2 text-md">
+            E-mail
+          </label>
+          <input
+            id="email"
+            type="email"
+            className="w-full rounded-md max-w-xs py-1.5 px-2 focus:outline-hot-pink text-md"
+            value={enteredEmail}
+            onChange={enteredEmailChangeHandler}
+          />
+          <label htmlFor="email" className="block my-2 text-md">
+            Password
+          </label>
+          <input
+            id="password"
+            type="password"
+            className="w-full rounded-md max-w-xs py-1.5 px-2 focus:outline-hot-pink text-md"
+            value={enteredPassword}
+            onChange={enteredPasswordChangeHandler}
+          />
+          <button className="bg-hot-pink hover:bg-pink text-white hover:text-black dark:bg-pink dark:hover:bg-hot-pink dark:hover:text-black w-full text-lg max-w-xs px-1.5 py-2 rounded-md mt-4">
+            Log In
+          </button>
+        </form>
+      </div>
       {isLoggedIn && (
         <button
           className="transition-all bg-hot-pink hover:bg-pink text-white hover:text-black dark:bg-pink dark:hover:bg-hot-pink dark:hover:text-black w-10/12 text-lg max-w-xs px-1.5 py-2 rounded-md mt-4"
@@ -100,7 +84,7 @@ const Form = ({ isLoggedIn, setIsLoggedIn, onLogin }) => {
           Logout
         </button>
       )}
-    </div>
+    </>
   );
 };
 
--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,4 +1,13 @@
+import { useState } from "react";
+import Form from "./Form";
+
 const Landing = () => {
+  const [openForm, setOpenForm] = useState(false);
+
+  const openFormHandler = () => {
+    setOpenForm(true);
+  };
+
   return (
     <div className="flex flex-col flex-grow items-center">
       <h1 className="text-4xl lg:text-5xl px-3 pt-28 sm:pt-32 md:pt-40 pb-2 text-black dark:text-white">
@@ -8,12 +17,16 @@ const Landing = () => {
         Here you can keep your list of 30 things YOU want to do before YOU turn
         30, track progress and see how far have you come.
       </p>
-      <button
-        type="button"
-        className="flex flex-wrap justify-center text-center w-10/12 text-lg bg-pink hover:bg-hot-pink dark:bg-hot-pink dark:hover:bg-pink text-black hover:text-white dark:text-white dark:hover:text-black max-w-xs px-3.5 py-2 mt-4 rounded-md transition-all"
-      >
-        Login/Register
-      </button>
+      {!openForm && (
+        <button
+          type="button"
+          className="flex flex-wrap justify-center text-center w-10/12 text-lg bg-pink hover:bg-hot-pink dark:bg-hot-pink dark:hover:bg-pink text-black hover:text-white dark:text-white dark:hover:text-black max-w-xs px-3.5 py-2 mt-4 rounded-md transition-all"
+          onClick={openFormHandler}
+        >
+          Login/Register
+        </button>
+      )}
+      {openForm && <Form />}
     </div>
   );
 };

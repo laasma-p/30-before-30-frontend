633f5c01ed38d86d44ecf03477f964cfe2b2375e
Lasma Poksane <laasmapoksane@gmail.com>
2024-10-19 22:35:31 +0200

    update Header component's content

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,12 +2,12 @@ const Header = () => {
   return (
     <header className="transition-all bg-pink dark:bg-hot-pink dark:text-white flex flex-col justify-center items-center text-center">
       <h1 className="w-10/12 text-2xl md:w-9/12 lg:text-3xl px-3 pt-6 pb-2">
-        30 Things I Want To Do Before Turning 30
+        30 Things To Do Before Turning 30
       </h1>
-      <p className="w-10/12 text-md md:w-9/12 lg:text-lg px-2 pb-4">
-        Here are some cool things that I want to do in my life before turning
-        30, but just still have not managed to do. Eventually they will be
-        crossed out so progress can be tracked on how much I have done.
+      <p className="w-8/12 text-md md:w-7/12 lg:text-lg px-2 pb-4">
+        Here you can see your list of 30 things to do in your life before 30. By
+        clicking on an item it gets crossed out, and progress updates to see how
+        far have you come.
       </p>
     </header>
   );

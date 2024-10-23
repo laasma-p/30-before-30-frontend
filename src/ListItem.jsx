4b0e00c242a1a2edcd40f751448a00c0f5552a1e
Lasma Poksane <laasmapoksane@gmail.com>
2024-10-23 13:26:50 +0200

    edit items and save the changes

--- src/ListItem.jsx
+++ src/ListItem.jsx
@@ -1,27 +1,68 @@
-const ListItem = ({ children, isCompleted, onClick, onRemove }) => {
+import { useState } from "react";
+
+const ListItem = ({ children, isCompleted, onClick, onRemove, onEdit }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedItem, setEditedItem] = useState(children);
+
   const handleClick = () => {
     if (!isCompleted) {
       onClick();
     }
   };
 
+  const saveEditHandler = (event) => {
+    event.preventDefault();
+    onEdit(editedItem);
+    setIsEditing(false);
+  };
+
+  const editChangeHandler = (event) => {
+    setEditedItem(event.target.value);
+  };
+
   return (
     <li className="py-2.5 px-3 text-lg w-full sm:max-w-sm flex justify-between items-center bg-light-pink rounded-lg shadow-lg mb-3 text-black">
-      <span
-        className={`break-all ${isCompleted ? "line-through" : " "}`}
-        onClick={handleClick}
-        style={{ cursor: !isCompleted ? "pointer" : "default" }}
-      >
-        {children}
-      </span>
-      <button
-        onClick={() => {
-          onRemove();
-        }}
-        className="ml-4 bg-hot-pink text-white hover:text-black px-3 py-1 rounded hover:bg-pink transition-all"
-      >
-        Remove
-      </button>
+      {isEditing ? (
+        <form onSubmit={saveEditHandler} className="w-full flex justify-center">
+          <input
+            type="text"
+            value={editedItem}
+            onChange={editChangeHandler}
+            className="border border-hot-pink p-1 rounded-md focus:outline-hot-pink mr-2"
+          />
+          <button className="bg-hot-pink text-white hover:text-black px-3 py-1 rounded hover:bg-pink transition-all">
+            Save
+          </button>
+        </form>
+      ) : (
+        <>
+          <span
+            className={`break-all ${isCompleted ? "line-through" : " "}`}
+            onClick={handleClick}
+            style={{ cursor: !isCompleted ? "pointer" : "default" }}
+          >
+            {children}
+          </span>
+          <div className="flex flex-row">
+            <button
+              onClick={() => {
+                setIsEditing(true);
+              }}
+              className="ml-4 bg-hot-pink text-white hover:text-black px-3 py-1 rounded hover:bg-pink transition-all"
+            >
+              Edit
+            </button>
+            <button
+              onClick={() => {
+                onRemove();
+              }}
+              className="ml-4 bg-hot-pink text-white hover:text-black px-3 py-1 rounded hover:bg-pink transition-all"
+            >
+              Remove
+            </button>
+          </div>
+        </>
+      )}
     </li>
   );
 };
